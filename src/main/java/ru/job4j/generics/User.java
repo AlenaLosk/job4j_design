fd3ad006697dd47bddb4697f38a9ab5db99a41ba
Kowalski42 <Elena.Loskutnikov.a@mail.ru>
2021-11-13 14:58:55 +0300

    5.2.2. Реализовать Store<T extends Base> [#157]

---
+++ src/main/java/ru/job4j/generics/User.java
@@ -0,0 +1,55 @@
+package ru.job4j.generics;
+
+public class User {
+    private String name;
+    private String role;
+    private int age;
+
+    public User(String name, String role, int age) {
+        this.name = name;
+        this.role = role;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        if (getAge() != user.getAge()) return false;
+        if (!getName().equals(user.getName())) return false;
+        return getRole().equals(user.getRole());
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getName().hashCode();
+        result = 31 * result + getRole().hashCode();
+        result = 31 * result + getAge();
+        return result;
+    }
+}
