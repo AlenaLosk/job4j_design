3095fcc2d91367369185e01f01b47f0351af396e
Kowalski42 <Elena.Loskutnikov.a@mail.ru>
2021-11-25 00:12:26 +0300

    7. ListIterator [#350217 #232177]

--- src/main/java/ru/job4j/iterator/ListUtils.java
+++ src/main/java/ru/job4j/iterator/ListUtils.java
@@ -8,26 +8,19 @@ public class ListUtils {
     public static <T> void addBefore(List<T> list, int index, T value) {
         Objects.checkIndex(index, list.size());
         ListIterator<T> i = list.listIterator(index);
-        if (i.hasPrevious()) {
-            i.previousIndex();
-            i.add(value);
-        }
+        i.add(value);
     }
 
     public static <T> void addAfter(List<T> list, int index, T value) {
         Objects.checkIndex(index, list.size());
-        ListIterator<T> i = list.listIterator(index);
-        if (i.hasNext()) {
-            i.next();
-            i.add(value);
-        }
+        ListIterator<T> i = list.listIterator(index + 1);
+        i.add(value);
     }
 
     public static <T> void removeIf(List<T> list, Predicate<T> filter) {
         ListIterator<T> i = list.listIterator();
         while (i.hasNext()) {
-            T tempI = i.next();
-            if (filter.test(tempI)) {
+            if (filter.test(i.next())) {
                 i.remove();
             }
         }
@@ -36,26 +29,19 @@ public class ListUtils {
     public static <T> void replaceIf(List<T> list, Predicate<T> filter, T value) {
         ListIterator<T> i = list.listIterator();
         while (i.hasNext()) {
-            T tempI = i.next();
-            if (filter.test(tempI)) {
+            if (filter.test(i.next())) {
                 i.set(value);
             }
         }
     }
 
     public static <T> void removeAll(List<T> list, List<T> elements) {
-        ListIterator<T> j = elements.listIterator();
-        while (j.hasNext()) {
-            T tempJ = j.next();
-            ListIterator<T> i = list.listIterator();
-            while (i.hasNext()) {
-                T tempI = i.next();
-                if (tempI.equals(tempJ)) {
-                    i.remove();
-                    break;
-                }
+        ListIterator<T> i = list.listIterator();
+        while (i.hasNext()) {
+            T tempI = i.next();
+            if (elements.stream().anyMatch(e -> e.equals(tempI))) {
+                i.remove();
             }
         }
     }
-
 }
